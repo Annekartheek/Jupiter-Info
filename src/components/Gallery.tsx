8c3f383c2b9852a720a13b2e27d32814949f75a3
Annekartheek <karthikanne12@gmail.com>
2024-12-28 10:35:18 +0530

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -13,7 +13,7 @@ export function Gallery() {
       description: "Jupiter's iconic storm that has been raging for centuries"
     },
     {
-      url: "https://www.shutterstock.com/image-illustration/planet-jupiter-some-69-known-moons-1115051534",
+      url: "https://www.shutterstock.com/image-vector/jupiter-moons-vector-educational-poster-scientific-1385322980",
       title: "Jupiter and Its Moons",
       description: "The gas giant and some of its largest Galilean moons"
     }
